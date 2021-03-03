b1eb4d0c92b8eb16f3308e4d87da3a40bd6241f8
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-03-03 21:45:02 +0100

    crazy test 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import HeroImg1 from "./components/HeroImg1";
-import Info from "./components/Info";
-import Register from "./components/Register";
+import styled from 'styled-components';
+
+const Barra = styled.div`
+  width: 80%;
+  margin: 10px auto auto auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  :last-child {
+    border-bottom: solid green;
+  }
+  
+
+`
+
+const Img = styled.img`
+  height: 100px;
+  margin-left: 5px;
+`
 
 function App() {
   return (
     <>
-      <HeroImg1 />
-      <Info />
-      <Register />
+      <Barra>
+        <Img src="http://www.dooblocawi.com/client/AssemblyCache/Atc_639f4647-0b6b-420a-a4d3-b55160ffadf4_0.png" alt=""/>
+        <Img src="http://www.dooblocawi.com/client/AssemblyCache/Atc_7fe28d3d-4032-43c3-94f2-cb32fb604290_0.png" alt=""/>
+        <Img src="http://www.dooblocawi.com/client/AssemblyCache/Atc_bb2686fe-f3c5-4351-acd1-51eb383385ff_0.png" alt=""/>
+      </Barra>
+      <Barra>
+        <Img src="http://www.dooblocawi.com/client/AssemblyCache/Atc_50492416-0909-4f22-b8ea-d82fe8f0bed4_0.jpeg" alt=""/>
+        <Img src="http://www.dooblocawi.com/client/AssemblyCache/Atc_faa5b74a-46ec-47cf-ae94-ddc8de6cdfba_0.png" alt=""/>
+      </Barra>     
     </>
   );
 }
